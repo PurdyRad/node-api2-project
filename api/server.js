d3e90b4955018a45b6a0539a8881d8084a69facb
Adam Purdy <purdyrad@Adams-MacBook-Air.local>
2021-04-06 14:39:40 -0400

    set up basic port

--- api/server.js
+++ api/server.js
@@ -1,2 +1,11 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req,res) => {
+    res.send(`<h3>Ya found da Source!</h3>`);
+}); 
+module.exports = server;
