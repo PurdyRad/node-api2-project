2aadea6c81c279e1ca9998a1a7ceb60fd0874f1b
Adam Purdy <purdyrad@Adams-MacBook-Air.local>
2021-04-07 11:23:04 -0400

    all passing tests

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -57,15 +57,29 @@ route.post('/', (req,res) => {
 route.put('/:id', (req,res) => {
     const id = req.params.id;
     const updatedPost = req.body;
-    if(!id){res.status(404).json({ message: "The post with the specified ID does not exist" })}
     if(!updatedPost.title || !updatedPost.contents){
-        res.status(400).json({message: "Please provide title and contents for the post"})
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
     } else {
-        Post.update(id, updatedPost)   
+        Post.findById(id)   
+        .then(post => {
+            if(!post){
+                res.status(404).json({
+                    message: 'The post with the specified ID does not exist.'
+                })
+            } else {
+               return Post.update(id, updatedPost)
+            }
+        })
+        .then(data => {
+            if (data) {
+                return Post.findById(id)
+            }
+        })
         .then(post => {
             if(post){
-                res.status(200)
-                return updatedPost
+                res.json(post)
             }
         })
         .catch(err => {
@@ -75,43 +89,32 @@ route.put('/:id', (req,res) => {
 }});
 
 //DELETE
-route.delete('/:id', (req,res) => {
-    const id = req.params.id;
-    if(!id){res.status(404).json({message: "The post with the specified ID does not exist"})}
-    Post.remove(id)
-    .then(posts => {
-        if(posts)
-        {res.status(200).json({message: 'Ya done obliterated the post!'})}
-    })
-    .catch(err => {
-        console.error('Geterr', err)
-        res.status(500).json({message:"The posts could not be deleted" })
-    });
+route.delete('/:id', async (req,res) => {
+   try{
+    const post = await Post.findById(req.params.id)
+    if(!post){
+        res.status(404).json({message: 'The post with the specified ID does not exist.'})
+    }
+    else {
+        await Post.remove(req.params.id)
+        res.json(post)
+    }
+   } catch(err) {
+    console.error('Geterr', err)
+    res.status(500).json({message:"The posts could not be deleted" })
+    }
 });
 
 //GET COMMENT
-// route.get('/:id/comments', (req,res) => {
-//     const id = req.params.id;
-//     if(!id){res.status(404).json({ message: "The post with the specified ID does not exist" })}
-//     Post.findCommentById(id)
-//     .then(comments => {
-//         console.log('COMMENTIDPOSTS', comments)
-//         res.status(200).json(comments)
-//     })
-//     .catch(err => {
-//         console.error('Geterr', err)
-//         res.status(500).json({message:"The comments information could not be retrieved" })
-//     });
-// });
 route.get('/:id/comments', async (req,res) => {
     try{
     const id = req.params.id;
-    const commentsArr = await Post.findCommentById(id)
-    if(!commentsArr){
+    const post = await Post.findById(id)
+    if(!post){
         res.status(404).json({ message: "The post with the specified ID does not exist" })
     } else{
-
-        res.status(200).json(commentsArr)
+        const comments = await Post.findPostComments(id)
+        res.json(comments)
     }}
     catch(err) {
         console.error('Geterr', err)
@@ -119,24 +122,5 @@ route.get('/:id/comments', async (req,res) => {
     }
 }
 );
-
-
-// route.post('/', (req,res) => {
-//     console.log('req.body:', req.body)
-//     const newPost = req.body;
-//         Post.insertComment(newPost)   
-//         .then(cId => {
-//             const id = cId.id;
-//             return Post.findCommentById(id)
-//         })
-//         .then(post => {
-//             console.log('commentPOST', post)
-//             res.status(201).json(post)
-//         })
-//         .catch(err => {
-//             console.error('Posterr', err)
-//             res.status(500).json({message:"There was an error while saving the post to the database." })
-//         });
-// });
 
 module.exports = route;
